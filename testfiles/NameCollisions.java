be7fdd02b6ab02811bbfb60d859291a7681483a7
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2022-04-10 01:17:55 -0700

    Added test for reserved name handling

---
+++ testfiles/NameCollisions.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NameCollisions {
+
+  Map<String, Integer> fruitTypes;
+
+  // `map` is a reserved keyword
+  Map<String, Integer> map;
+
+  // Since `type` is a reserved keyword in Go, this should fail
+  public int getFruit(String type) {
+    return this.fruitTypes.get(type);
+  }
+
+  // This is also a collision, with the keyword `range`
+  public int[] range() {
+    int[] values = new int[this.map.size()];
+    int ind = 0;
+    for (int val : this.map.values()) {
+      values[ind] = val;
+      ind++;
+    }
+    return values;
+  }
+
+  public NameCollisions() {
+    // Another collision, but a little more subtle
+    this.map = new HashMap<>();
+  }
+
+  public static void main(String[] args) {
+    NameCollisions test = new NameCollisions();
+
+    // Even more collisions
+    Map<String, Integer> map = new HashMap<>();
+    map.put("Apple", 1);
+    map.put("Lemon", 4);
+
+    test.map = map;
+    System.out.println(Arrays.toString(test.range()));
+  }
+}
