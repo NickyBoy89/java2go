c5d8796405c1530f00a85c40e170a205f8df1506
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2024-12-09 18:57:42 -0800

    feat: Added tests for primitive type conversions

---
+++ tests/primitive_types_test.go
@@ -0,0 +1,58 @@
+package tests
+
+import "testing"
+
+func TestPrimitiveIntegralTypes(t *testing.T) {
+	javaInput := `class Foo {
+	byte a;
+	short b;
+	int c;
+	long d;
+	char e;
+}`
+
+	goOutput := `package main
+
+type Foo struct {
+	a	byte
+	b	int16
+	c	int32
+	d	int64
+	e	rune
+}
+`
+
+	ComparePrograms(javaInput, goOutput, t)
+}
+
+func TestFloatingPointTypes(t *testing.T) {
+	javaInput := `class Foo {
+	float a;
+	double b;
+}`
+
+	goOutput := `package main
+
+type Foo struct {
+	a	float32
+	b	float64
+}
+`
+
+	ComparePrograms(javaInput, goOutput, t)
+}
+
+func TestBooleanType(t *testing.T) {
+	javaInput := `class Foo {
+	boolean a;
+}`
+
+	goOutput := `package main
+
+type Foo struct {
+	a	bool
+}
+`
+
+	ComparePrograms(javaInput, goOutput, t)
+}
